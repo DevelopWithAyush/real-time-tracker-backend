83089f44c04478508fd53e937c4f7bc5337348a0
developwithayush <developwithdubey@gmail.com>
2024-07-07 14:48:32 +0530

    backend of real time tracker

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+const app = express();
+const port = 5000;
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    })
+);
+
+
+app.get("/", (req, res) => {
+  res.send("hey boy");
+});
+
+io.on("connection", (socket) => {
+  console.log(`user connect with ${socket.id}`);
+
+    socket.on("send-location", (data) => {
+      console.log(data.latitude, data.longitude)
+        io.emit("recive-location", { id: socket.id, ...data })
+    });
+
+  socket.on("disconnect", () => {
+    io.emit("leave",{id:socket.id})
+    console.log(`user disconnect which have socket id ${socket.id}`);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server listen on port ${port}`);
+});
