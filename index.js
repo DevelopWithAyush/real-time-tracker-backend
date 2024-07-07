dd32daa0c28034a89358678727e0f2e79b736d06
developwithayush <developwithdubey@gmail.com>
2024-07-07 14:51:34 +0530

    adding the url

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ import cors from "cors";
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://real-time-tracker-frontend.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
 });
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://real-time-tracker-frontend.vercel.app",
     })
 );
 
